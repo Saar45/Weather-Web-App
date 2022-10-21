686f0bae0472bdfecfb0149c24c014acd6e4e661
Saar45 <nabysarr16@gmail.com>
2022-10-21 17:44:18 +0200

    We have added a default port for the heroku servers to listen to

--- app.js
+++ app.js
@@ -216,6 +216,6 @@ app.post("/", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server is up and running!");
 });
